67596629b3a33b8d7409ebdf82a7bef5eabed0f9
Vallabh Kansagara <vrkansagara@gmail.com>
2023-02-16 23:52:22 +0530

    order change for the reference

--- other/config.h
+++ other/config.h
@@ -140,11 +140,8 @@ static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen 
 /* Mod4Mask    |    64 | Windows */
 /* Mod1Mask    |     8 | Alt */
 /* LockMask    |     2 | Caps Lock */
-/* ControlMask |     4 | Ctrl */
-/* Mod1Mask    |     8 | Alt */
 /* Mod2Mask    |    16 | Num Lock */
 /* Mod3Mask    |    32 | Scroll Lock */
-/* Mod4Mask    |    64 | Windows */
 /* Mod5Mask    |   128 | ??? */
 
 /* key definitions */
